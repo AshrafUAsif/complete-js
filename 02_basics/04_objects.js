d405665f49542ba53cb1af21afa30076a1f7038d
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2023-11-26 19:27:49 +0000

    Objects added

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+//const tinderUser = new Object() --> This is called singleton object
+
+const tinderUser = {} // --> THis is called non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Asif"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+// Object এর ভিতর Object
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "ashraf",
+            lastname: "uddin"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+//const obj3 = {obj1, obj2}
+// Object assign for merging two or more objects
+
+//const obj3 = Object.assign({}, obj1, obj2)
+
+// merging can be done by spread operator
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+// Database এর value আসলে 
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "h@gmail.com"
+    },
+    {
+        id: 3,
+        email: "h@gmail.com"
+    },
+
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
