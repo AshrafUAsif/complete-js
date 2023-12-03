5327c00ef572f6ab70e4a941b7f73233ed7f0285
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2023-12-03 15:25:38 +0000

    Till map added

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -10,11 +10,32 @@ for (let i = 0; i < 10; i++) {
 
 // loop inside loop
 for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value: ${i}`);
+    //console.log(`Outer loop value: ${i}`);
     for (let j = 0; j <= 10; j++) {
         //console.log(`Inner loop value: ${j} and outer loop ${i}`);
-        console.log(i + '*' + j + ' = ' + i*j );
+        //console.log(i + '*' + j + ' = ' + i*j );
     }
+    
+}
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+// Break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        //break
+        continue
+    }
+    console.log(`Value of i is ${index}`);
     
 }
 
